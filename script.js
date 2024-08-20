633c9847eaf2c82cc51beb395bfcf70beb4120fc
Mykhailo Ulanovych <97468374+UlanovychM@users.noreply.github.com>
2024-08-20 23:57:22 +0300

    update

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 'use strict';
 
+const modalsBtn = document.querySelectorAll('.show-modal-window');
+
+modalsBtn.forEach(item => {
+  item.addEventListener('click', () => {
+    document.querySelector('.modal-window').classList.toggle('hidden');
+  });
+});
