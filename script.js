800a1a9b6137581cdef248a342ccbd22a2ef8624
Mykhailo Ulanovych <97468374+UlanovychM@users.noreply.github.com>
2024-08-21 12:04:34 +0300

    update

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 'use strict';
 
 const modalsBtn = document.querySelectorAll('.show-modal-window');
+const modalWindow = document.querySelector('.modal-window');
+const closeBtn = document.querySelector('.close-modal-window');
+
+const openModal = (elem, clas) => {
+  elem.classList.toggle(clas);
+};
+
+const closeModal = (elem, clas) => {
+  elem.classList.add(clas);
+};
+
+closeBtn.addEventListener('click', () => {
+  closeModal(modalWindow, 'hidden');
+});
+
+document.addEventListener('keydown', e => {
+  if (e.code === 'Escape' && !modalWindow.classList.contains('hidden')) {
+    closeModal(modalWindow, 'hidden');
+  }
+});
 
 modalsBtn.forEach(item => {
   item.addEventListener('click', () => {
-    document.querySelector('.modal-window').classList.toggle('hidden');
+    openModal(modalWindow, 'hidden');
   });
 });
